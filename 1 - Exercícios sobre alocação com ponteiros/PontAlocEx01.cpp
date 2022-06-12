fb3cf045363675883038b17282095c63708c6170
Diego Marques <64621051+diegomarques1@users.noreply.github.com>
2022-06-12 00:40:48 -0300

    Rename AlocPontEx01.cpp to PontAlocEx01.cpp

--- 1 - Exercícios sobre alocação com ponteiros/PontAlocEx01.cpp
+++ 1 - Exercícios sobre alocação com ponteiros/PontAlocEx01.cpp
@@ -1,15 +1,15 @@
-// Crie uma funÁ„o que receba como par‚metros dois vetores de inteiros: x1 e x2 e 
-// as suas respectivas quantidades de elementos: n1 e n2. A funÁ„o dever· retornar
-// um ponteiro para um terceiro vetor, x3, alocado dinamicamente, contendo a uni„o
+// Crie uma fun√ß√£o que receba como par√¢metros dois vetores de inteiros: x1 e x2 e 
+// as suas respectivas quantidades de elementos: n1 e n2. A fun√ß√£o dever√° retornar
+// um ponteiro para um terceiro vetor, x3, alocado dinamicamente, contendo a uni√£o
 // de x1 e x2 e usar o ponteiro qtd para retornar o tamanho de x3.
-// Sendo x1 = {1, 3, 5, 6, 7} e x2 = {1, 3, 4, 6, 8}; x3 ir· conter {1, 3, 4, 5, 7, 8}.
-// Assinatura da funÁ„o: int* uniao(int *x1, int *x2, int n1, int n2, int *qtd);
+// Sendo x1 = {1, 3, 5, 6, 7} e x2 = {1, 3, 4, 6, 8}; x3 ir√° conter {1, 3, 4, 5, 7, 8}.
+// Assinatura da fun√ß√£o: int* uniao(int *x1, int *x2, int n1, int n2, int *qtd);
 
 #include <iostream>
 
 using namespace std;
 
-int* uniao(int* x1, int* x2, int n1, int n2, int* qtd) // N„o consegui pensar em uma soluÁ„o que n„o fosse O(n^2)
+int* uniao(int* x1, int* x2, int n1, int n2, int* qtd) // N√£o consegui pensar em uma solu√ß√£o que n√£o fosse O(n^2)
 {
     *qtd = n2;
     bool flag = false;
